341f5bee80a5d04cf3707c296fe92d74f27b7967
Alisha Kareemulla <33855600+alishakareem@users.noreply.github.com>
2022-12-07 23:23:10 -0500

    changed to azure

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   devServer:{
     // proxy:'http://localhost:1234/',
-    // proxy:'https://alisha-backend.azurewebsites.net/',
-    proxy:'https://alisha-backend.herokuapp.com/',
+    proxy:'https://alisha-backend.azurewebsites.net/',
+    // proxy:'https://alisha-backend.herokuapp.com/',
   },
 }
